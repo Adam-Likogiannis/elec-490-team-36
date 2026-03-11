418cfada9c225a6ad9791e2b1927227da7e4b53c
Kevin Xiong <156862330+21waffle@users.noreply.github.com>
2026-03-10 21:30:37 -0400

    Updated Robustness testing
    Can now watermark and decode using new system. Also added jpeg compression as a test option.

--- test_modifications.py
+++ test_modifications.py
@@ -1,6 +1,8 @@
 import numpy as np
 from PIL import Image
 import embed_new
+import cameraEmbed
+
 def decode_image(inputpath):
     step = 30
     key = ""
@@ -12,7 +14,21 @@ def watermark_image(message, inputpath, outputpath):
     quality = 95
     embed_new.apply_watermark_to_camera_image(message, step, key, inputpath, outputpath, quality)
 
-def process_image_pct(input_path, output_path, crop_pct=10, degrees=15, sigma=0.05):
+
+
+def watermark_image_new(message, inputpath, outputpath):
+    step = 30
+    key = None
+    quality = 95
+    cameraEmbed.watermark_camera_image(message, step, key, inputpath, outputpath, quality)
+
+
+def decode_image_new(inputpath):
+    step = 30
+    key = ""
+    message = cameraEmbed.decode_image(inputpath, step, key)
+
+def process_image_pct(input_path, output_path, crop_pct=10, degrees=15, sigma=0.05, quality = 85):
     with Image.open(input_path) as img:
         w, h = img.size
         
@@ -31,16 +47,24 @@ def process_image_pct(input_path, output_path, crop_pct=10, degrees=15, sigma=0.
         noise = np.random.normal(0, sigma, img_array.shape)
         noisy_img = np.clip(img_array + noise, 0, 255).astype(np.uint8)
 
+        final_img = Image.fromarray(noisy_img)
+        final_img.save(output_path, "JPEG", quality=quality)
+        
         # Save
         Image.fromarray(noisy_img).save(output_path)
         print(f"Success!")
 
 if __name__ == "__main__":
-    file_name = "WM_new.jpg"
+    file_name = "lights_watermarked.jpg"
     input_image = "./Old_Images/" + file_name
-    output_image ="./Robust_test/WM_new_noise.jpg" 
+    output_image ="./Robust_test/lights_compression.jpg" 
 
     #watermark_image("YAY MESESAGE", input_image, output_image)
-    process_image_pct(input_image, output_image, crop_pct=0, degrees=0, sigma=0.05)
-    decode_image(output_image)
+    #process_image_pct(input_image, output_image, crop_pct=0, degrees=0, sigma=0.05)
+    #decode_image(output_image)
+    
+    #watermark_image_new("YAY MESESAGE", input_image, output_image)
+    process_image_pct(input_image, output_image, crop_pct=0, degrees=0, sigma=0, quality = 85)
+    decode_image_new(output_image)
+    
     
